2416466bc2bf7244471a3157b676da76936c75e6
rpachotome <rpachotome@eniac32.danielcastelao.org>
2014-10-06 09:26:05 +0200

    ABC

--- src/boletin03/Boletin03.java
+++ src/boletin03/Boletin03.java
@@ -8,13 +8,14 @@ public class Boletin03 {
   
     public static void main(String[] args) {
      //Introducir datos por teclado  
+        System.out.println("¿Esta usted ansioso por saber el área de su rectángulo?\n ¿Es usted de los que el cálculo les espanta?\n Pues use esta aplicación:");
         float base, altura;
-        System.out.println("Base:");
+        System.out.println(" Base:");
         Scanner dato = new Scanner(System.in);
         base=dato.nextFloat();
         System.out.println("altura:");
         altura=dato.nextFloat();
-        System.out.println("Area="+(base*altura)+" por cierto, Aitor es gay");
+        System.out.println("Area="+(base*altura));
     }
     
 }
